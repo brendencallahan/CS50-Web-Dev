06e0465875ebf3738a83c1abbf79406d6ca19836
Brenden Callahan <brendenkcallahan@gmail.com>
2022-11-27 10:30:02 -0800

    Added a few other routes/functions to explore how Django works

--- cs50apps/wiki/urls.py
+++ cs50apps/wiki/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("name", views.name, name="name")
+    path("name", views.name, name="name"),
+    path("david", views.david, name="david"),
+    path("<str:name>", views.greet, name="greet")
 ]
--- cs50apps/wiki/views.py
+++ cs50apps/wiki/views.py
@@ -7,3 +7,9 @@ def index(request):
 
 def name(request):
     return HttpResponse("name")
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}")
