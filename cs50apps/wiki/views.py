fd578874c1ec7835e85e361c8422ea40c81515dc
Brenden Callahan <brendenkcallahan@gmail.com>
2022-11-27 12:31:55 -0800

    Added template and boilerplate html

--- cs50apps/wiki/views.py
+++ cs50apps/wiki/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "hello/index.html")
+    return render(request, "wiki/index.html")
 
 def greet(request, name):
     return HttpResponse(f"Hello, {name.capitalize()}")
