7f297ceb5d2cd0e933d943297cd5a6825d01483d
Brenden Callahan <brendenkcallahan@gmail.com>
2022-12-06 09:42:03 -0800

    Added ability to save new files

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,18 @@
+import os
+import markdown2
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django import forms
 
 from . import util
 
 # hmmm
+
+class MyForm(forms.Form):
+    user_markdown = forms.CharField()
+    user_title = forms.CharField()
+
 
 def index(request):
 
@@ -18,6 +26,7 @@ def index(request):
     
     elif search_text not in entries:
 
+        # Search for entries that contain substring of search and filter the rest
         culled_entries = list(filter(lambda x: search_text in x, entries))
         return render(request, "encyclopedia/search_results.html", {
             "entries": culled_entries
@@ -29,6 +38,8 @@ def index(request):
 
 def mdrender(request, entry_name):
     markdown_file = util.get_entry(entry_name)
+
+    # Get markdown file if it exists otherwise return an error
     if markdown_file is not None:
         return render(request, "encyclopedia/generic_entry.html", {
             "markdown": markdown_file
@@ -37,9 +48,23 @@ def mdrender(request, entry_name):
         return render(request, "encyclopedia/error_markdown_file_not_found.html")
 
 def create_page(request):
+
+    # Validate form method and info
     if request.method == 'POST':
-        return render(request, "")
+        form = MyForm(request.POST)
+        if form.is_valid():
+            user_markdown = form.cleaned_data['user_markdown']
+            user_title = form.cleaned_data['user_title']
+
+            util.save_entry(user_title, user_markdown)
+
+            return HttpResponseRedirect(reverse("index"))
+
+        
 
     else:
+        form = MyForm()
         return render(request, "encyclopedia/create_page.html", {
-        }) 
+            'form': form
+        })
+
