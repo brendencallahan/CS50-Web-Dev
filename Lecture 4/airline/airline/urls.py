1e10859b51668914bf6b07079036b3d7fc07c5ed
Brenden Callahan <brendenkcallahan@gmail.com>
2022-12-14 15:21:43 -0800

    Fixed minor typos and migrated db changes

--- Lecture 4/airline/airline/urls.py
+++ Lecture 4/airline/airline/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('flights/', include("flights.url"))
+    path('flights/', include("flights.urls"))
 ]
