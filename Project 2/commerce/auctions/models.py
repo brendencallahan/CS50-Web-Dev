2c9c2d9605fd5e97cc260cbeb6101522f5dcd56c
Brenden Callahan <brendenkcallahan@gmail.com>
2022-12-18 09:40:54 -0800

    Changed listing model

--- Project 2/commerce/auctions/models.py
+++ Project 2/commerce/auctions/models.py
@@ -2,26 +2,42 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+LISTING_CATEGORIES = (
+    ("BOOKS", "Books"),
+    ("BUSINESS", "Business & Industrial"),
+    ("CLOTHES", "Clothing, Shoes & Accessories"),
+    ("COLLECTIBLES", "Collectibles"),
+    ("ELECTRONICS", "Consumer Electronics"),
+    ("CRAFTS", "Crafts"),
+    ("DOLLS", "Dolls & Bears"),
+    ("HOME", "Home & Garden"),
+    ("MOTORS", "Motors"),
+    ("PETS", "Pet Supplies"),
+    ("SPORTS GOODS", "Sporting Goods"),
+    ("SPORTS MEM", "Sports Mem, Cards & Fan Shop"),
+    ("TOYS", "Toys & Hobbies"),
+    ("ANTIQUES", "Antiques"),
+    ("COMPUTERS", "Computers/Tablets & Networking")
+)
+
 class User(AbstractUser):
     pass
 
-class Listing(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.CharField(max_length=1200)
-    bids = models.ForeignKey(Bids, on_delete=models.CASCADE)
-    categories = models.ForeignKey(Category, on_delete=models.PROTECT)
-    picture = models.URLField(max_length=200)
-    comments = models.ForeignKey(Comments, on_delete=models.CASCADE)
-
-
-class Bids(models.Model):
-    bid = models.IntegerField()
-
-class Comments(models.Model):
+class Bid(models.Model):
+    bid_amount = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    comment = models.CharField(max_length=500)
     date = models.DateTimeField(auto_now=False, auto_now_add=True)
 
-class Category(models.Model):
-    # Add a list to choose from
-    pass
+class Comment(models.Model):
+    comment = models.CharField(max_length=500)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+class Listing(models.Model):
+    picture = models.URLField()
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=1200)
+    category = models.CharField(max_length=50, choices=LISTING_CATEGORIES, default=None)
+    bids = models.ForeignKey(Bid, on_delete=models.CASCADE)
+    comments = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    user = models.ManyToManyField(User, related_name="user_listings")
