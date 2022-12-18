885b1d64738a7851d27c68d959b1977799e75ca0
Brenden Callahan <brendenkcallahan@gmail.com>
2022-12-17 23:19:42 -0800

    Finished most models

--- Project 2/commerce/auctions/models.py
+++ Project 2/commerce/auctions/models.py
@@ -11,13 +11,17 @@ class Listing(models.Model):
     bids = models.ForeignKey(Bids, on_delete=models.CASCADE)
     categories = models.ForeignKey(Category, on_delete=models.PROTECT)
     picture = models.URLField(max_length=200)
+    comments = models.ForeignKey(Comments, on_delete=models.CASCADE)
 
 
 class Bids(models.Model):
     bid = models.IntegerField()
 
 class Comments(models.Model):
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=500)
+    date = models.DateTimeField(auto_now=False, auto_now_add=True)
 
 class Category(models.Model):
+    # Add a list to choose from
     pass
