b7428b68c2edd1e7f94d3a20465d12d5d9a16847
Brenden Callahan <brendenkcallahan@gmail.com>
2022-12-17 23:12:55 -0800

    Made first listing model

--- Project 2/commerce/auctions/models.py
+++ Project 2/commerce/auctions/models.py
@@ -6,10 +6,18 @@ class User(AbstractUser):
     pass
 
 class Listing(models.Model):
-    pass
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=1200)
+    bids = models.ForeignKey(Bids, on_delete=models.CASCADE)
+    categories = models.ForeignKey(Category, on_delete=models.PROTECT)
+    picture = models.URLField(max_length=200)
+
 
 class Bids(models.Model):
-    pass
+    bid = models.IntegerField()
 
 class Comments(models.Model):
+    pass
+
+class Category(models.Model):
     pass
